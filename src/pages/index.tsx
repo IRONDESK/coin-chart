9e50e716bca41f35ec3fb5aed5f958dcc2205b39
IRONDESK <zerozerobase@gmail.com>
2022-08-10 13:45:34 +0900

    :tada: initial setting and commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next"
+import Chart from "../components/Chart/Chart"
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Chart />
+    </>
+  )
+}
+
+export default Home
